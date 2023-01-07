b87bdf2656dc7f5137bdc9029f3a98a7ad0a1691
Todor <todormarinov02@gmail.com>
2023-01-07 17:49:50 +0200

    added migrations, dbcontext and some code fixes

--- BooksProject/Context/AppDbContext.cs
+++ BooksProject/Context/AppDbContext.cs
@@ -10,7 +10,6 @@ namespace BooksProject.Context
         {
             
         }
-
         public DbSet<Book> Books { get; set; }
         public DbSet<Author> Authors { get; set; }
         public DbSet<AuthorBook> AuthorBooks { get; set; }
